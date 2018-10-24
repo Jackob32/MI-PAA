9b92b013359afbb6764be7010449c51c83e97f3f
trhlijak <trhlijak@fit.cvut.cz>
2018-10-24 18:00:36 +0200

    the heuristics algorithm finished

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -31,69 +31,78 @@ BackpackProblem *bruteForceCalculate(BackpackProblem *problem, int M, int i) {
     }
 }
 
+
+
+
+int *bruteForceCalculate2(int * m, int * c, int * res, int M, int W, int n, int i) {
+
+    //pokud jsem dosel na konec plneni knapsacku
+    if (i >= n || W == M) return res;
+
+    if (W + m[i] > M) {
+        return bruteForceCalculate2(m,c,res, M,W,n, i + 1);
+    }
+
+    int * resAdded=copy(res);
+
+    int  *tmp1 = bruteForceCalculate2(problem->flipValue(i), M, i + 1);
+    int  *tmp2 = bruteForceCalculate2(problem, M, i + 1);
+
+    if (tmp1->getPrice() > tmp2->getPrice()) {
+        delete tmp2;
+        return tmp1;
+    } else {
+        delete tmp1;
+        return tmp2;
+    }
+}
+
+
 BackpackProblem *heuristicsCalculate(BackpackProblem *problem, int M, int i) {
 
-    double *ratio = new double[problem->getN()];
-
     array<double, 2> a[problem->getN()];
-    /*= {{20, 11},
-                          {10, 20},
-                          {39, 14},
-                          {29, 15},
-                          {22, 23}};
-                          */
-
-
 
     for (int j = 0; j < problem->getN(); ++j) {
 
-        a[j] = {(double) problem->getW(j) / ((double) problem->getP(j)),(double)j};
+        a[j] = {(double) problem->getW(j) / ((double) problem->getP(j)), (double) j};
 
     }
 
     sort(a, a + problem->getN());
 
-    int k=0;
+    int k = 0;
 
-    while(k<problem->getN()) {
+    while (k < problem->getN()) {
 
         if (problem->getM() + problem->getW((int) a[k][1]) <= problem->getinitM()) {
 
-        problem->itemNum[(int) a[k][1]] = 1;
-        problem->M += problem->getW((int) a[k][1]);
-        problem->Price += problem->getP((int) a[k][1]);
-    }
+            problem->itemNum[(int) a[k][1]] = 1;
+            problem->M += problem->getW((int) a[k][1]);
+            problem->Price += problem->getP((int) a[k][1]);
+        }
         k++;
 
     }
 
     /*
     cout <<"\n\n\n";
-
-
     for (int j = 0; j < problem->getN(); ++j) {
-
         cout << a[j][0] <<"  " << a[j][1] << " "<<  problem->getW((int)a[j][1]) << " " <<problem->getP((int)a[j][1]) << endl;
     }
 */
 
 
     return problem;
-
 }
 
 
-int main(int argc, char **argv) {
-    cout << "The Backpack Problem" << endl;
+int knap(string file, int modheur, int modoptim, int modcompare) {
 
-    string inst = argv[1];
-    //string inst = "../inst/knap_4.inst.dat";
-
-    cout << "Jmeno souboru " << inst << endl;
+   // cout << "Jmeno souboru " << file << endl;
 
     ifstream infile;
 
-    infile.open(inst);
+    infile.open(file);
 
     if (!infile) {
         cout << "Unable to open file";
@@ -102,7 +111,7 @@ int main(int argc, char **argv) {
     string line;
 
     const clock_t begin_time = clock();
-    cout << "begin time" << endl;
+
 // do something
 
     while (getline(infile, line)) {
@@ -111,7 +120,7 @@ int main(int argc, char **argv) {
         int id, n, M = 0;
 
         if (!(iss >> id >> n >> M)) {
-            cout << "Chyba formatu " << inst << endl;
+            cout << "Chyba formatu " << file << endl;
             break;
         } // error
 
@@ -127,43 +136,40 @@ int main(int argc, char **argv) {
 
             itemnum++;
             if (itemnum > n) {
-                cout << "Chyba formatu, vice itemu nez deklarovano " << inst << endl;
+                cout << "Chyba formatu, vice itemu nez deklarovano " << file << endl;
                 break;
             }
         }
+
         int SumWeights = 0;
 
         for (int i = 0; i < n; ++i) {
             SumWeights += Prices[i];
         }
+        BackpackProblem *heur = NULL;
+        if (modheur) {
+            heur = new BackpackProblem(id, n, M, Weights, Prices, SumWeights);
+            heur = heuristicsCalculate(heur, heur->getinitM(), 0);
+            if(modheur==2)
+            heur->format();
+        }
+        BackpackProblem *optim = NULL;
+        if (modoptim) {
+            optim = new BackpackProblem(id, n, M, Weights, Prices, SumWeights);
+            optim = bruteForceCalculate(optim, optim->getinitM(), 0);
+            //  heuristicsCalculate(tmp, tmp->getinitM(), 0);
+            if(modoptim==2)
+            optim->format();
+        }
 
-        BackpackProblem *heur = new BackpackProblem(id, n, M, Weights, Prices, SumWeights);
-        //       cout << "============================" << endl;
+        if (modoptim && modheur && modcompare) {
+            cout<<optim->id<<"  "<<(double)(optim->getPrice()-heur->getPrice())/optim->getPrice()*100<<endl;
+        }
 
-        heur=heuristicsCalculate(heur, heur->getinitM(), 0);
-
-
-        BackpackProblem *optim = new BackpackProblem(id, n, M, Weights, Prices, SumWeights);
-        //       cout << "============================" << endl;
-
-        optim=bruteForceCalculate(optim, optim->getinitM(), 0);
-
-        //  heuristicsCalculate(tmp, tmp->getinitM(), 0);
-
-
-        //cout<<tmp<<endl;
-        heur->format();
-
-        optim->format();
-
-
-
-
-        cout<<optim->id<<"  "<<(double)(optim->getPrice()-heur->getPrice())/optim->getPrice()*100<<endl;
-
-
-        delete heur;
-        delete optim;
+        if (modheur)
+            delete heur;
+        if (modoptim)
+            delete optim;
         delete[] Weights;
         delete[] Prices;
 
@@ -172,12 +178,53 @@ int main(int argc, char **argv) {
 
         BestPrice = 0;
     }
-    cout <<"\n"<< float(clock() - begin_time) / CLOCKS_PER_SEC << "  s "<<endl;
+    cout << file << "  "  <<float(clock() - begin_time) / CLOCKS_PER_SEC << " s " << endl;
 
     return 0;
 }
 
 
+int main(int argc, char **argv) {
 
+    cout << "The Backpack Problem" << endl;
+
+
+    knap("./inst/knap_4.inst.dat",0,1,0);
+    knap("./inst/knap_10.inst.dat",0,1,0);
+    knap("./inst/knap_20.inst.dat",0,1,0);
+    knap("./inst/knap_22.inst.dat",0,1,0);
+    knap("./inst/knap_25.inst.dat",0,1,0);
+
+
+
+
+    return 0;
+}
+
+
+/*
+heuristics only
+
+./inst/knap_4.inst.dat  0.000209  s
+./inst/knap_10.inst.dat  0.000421  s
+./inst/knap_20.inst.dat  0.000898  s
+./inst/knap_22.inst.dat  0.001005  s
+./inst/knap_25.inst.dat  0.00121  s
+./inst/knap_27.inst.dat  0.001313  s
+./inst/knap_30.inst.dat  0.00149  s
+./inst/knap_32.inst.dat  0.001455  s
+./inst/knap_35.inst.dat  0.001709  s
+./inst/knap_37.inst.dat  0.0018  s
+./inst/knap_40.inst.dat  0.001883  s
+
+
+./inst/knap_4.inst.dat  0.000172 s
+./inst/knap_10.inst.dat  0.006379 s
+./inst/knap_20.inst.dat  10.5503 s
+./inst/knap_22.inst.dat  39.0728 s
+./inst/knap_25.inst.dat  345.054 s
+
+
+ * */
 
 
